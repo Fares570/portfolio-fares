b62742317210dee687f1d82176e1ac81dcee81b9
Fares57 <fares.zeghadi@free.fr>
2026-06-11 22:31:39 +0200

    Modification section a propos

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,20 +6,22 @@ function About() {
 
       <div className="content-card">
         <p>
-          Après plusieurs années dans l’IT et la cybersécurité, j’ai choisi de renforcer
-          mes compétences techniques en développement web avec la formation Intégrateur Web
-          d’OpenClassrooms.
+          Après plus de six années d’expérience dans les domaines de l’informatique et de la cybersécurité, 
+          j’ai développé une solide compréhension des systèmes d’information, 
+          de la gestion de projet et des enjeux techniques liés à la sécurité des environnements numériques.
         </p>
 
         <p>
-          Cette formation m’a permis de réaliser des projets concrets en HTML, CSS,
-          JavaScript et React, tout en appliquant les bonnes pratiques de responsive design,
-          d’accessibilité, de SEO et de gestion de projet.
+          Souhaitant renforcer mon expertise technique et participer plus directement à la conception de solutions, 
+          j’ai complété mon parcours par une formation de Développeur Web au cours de laquelle j’ai réalisé plusieurs projets, 
+          en appliquant les bonnes pratiques de développement, d’accessibilité, de performance de responsive design et de backend.
         </p>
 
         <p>
-          Mon objectif est de créer des interfaces claires, fiables et professionnelles,
-          avec une attention particulière portée à la qualité du code et à l’expérience utilisateur.
+         Mon parcours me permet aujourd’hui de combiner des compétences en développement web, gestion de projet IT et cybersécurité. 
+         Curieux, rigoureux et orienté résultats, je recherche des opportunités dans le développement full-stack, les projets IT 
+         ou la cybersécurité, surtout où je pourrai mettre à profit mes compétence technique et en GRC pour contribuer à 
+         des projets à forte valeur ajoutée.
         </p>
       </div>
     </section>
